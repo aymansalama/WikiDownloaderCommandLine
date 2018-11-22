09cf78d09e2b3f4c2940fba85793729f2f4a39c8
Khaman <khamanconsolidate@gmail.com>
2018-11-22 01:25:29 +0000

    Change dates to latest, Change links of downloads, added md5sum check

--- wdcli.py
+++ wdcli.py
@@ -5,21 +5,24 @@ import os
 import sys
 import time
 import re       # regex
+import datetime
+import requests
+from requests.exceptions import HTTPError
 from urllib.request import urlopen
 ### TODOLIST
 # interactive
 # error generator
 # log
-# md5checker
+
 
 # class 
 def main():
     parser = argparse.ArgumentParser(description='Downloader of Wikimedia Dumps')
     parser.add_argument('-m', '--mirrors', nargs='?', type=int, help='Use mirror links instead of wikimedia. Such as 1:https://dumps.wikimedia.your.org 2:http://wikipedia.c3sl.ufpr.br', required=False)
-    parser.add_argument('-d', '--dates', nargs='?', help='Set the date of the dumps. Default = latest', required=False)
+    parser.add_argument('-d', '--dates', nargs='?', type=int, help='Set the date of the dumps. (e.g. 20181101). Default = 1st day of current month', required=False)
     parser.add_argument('-p', '--projects', help='Choose which wikimedia projects to download (e.g. all, wikipedia, wikibooks, wiktionary, wikimedia, wikinews, wikiversity, wikiquote, wikisource, wikivoyage)', required=False)
     parser.add_argument('-r', '--maxretries', help='Max retries to download a dump when md5sum doesn\'t fit. Default: 3', required=False)
-    parser.add_argument('-l', '--locales', nargs='+', help='Choose which language dumps to download (e.g)', required=False)
+    parser.add_argument('-l', '--locales', nargs='+', help='Choose which language dumps to download (e.g en my ar)', required=False)
     args = parser.parse_args()
     
     # Dumps Domain and Mirror
@@ -30,6 +33,7 @@ def main():
     elif args.mirrors == 2:
         dumpsdomain = 'http://wikipedia.c3sl.ufpr.br'
     else:
+        # Exception Handling for wrong input for mirror choices
         while True:
             secondChance = input("Invalid input for mirror choice.\nInput 1 for dumps.wikimedia.your.org\nInput 2 for wikipedia.c3sl.ufpr.br \
             \nInput 3 for default: dumps.wikimedia.your.org\n")
@@ -45,14 +49,25 @@ def main():
                 break   
                 
 
-    with urlopen('{}/backup-index.html'.format(dumpsdomain)) as url:
-        html = url.read().decode('utf-8')
 
-    # Dumps Date
+    # Dumps Date, default latest 
     if args.dates:
-        dates = args.dates 
+        # Input format exception handling
+        if  len(str(args.dates)) < 8 or\
+            len(str(args.dates)) > 8:
+                print(len(str(args.dates)))
+                print("\nWrong date format! Please enter as YYYYMMDD format.\n")
+                sys.exit(0)
+        # Stop if the date not in the past
+        if  args.dates > int(datetime.date.today().strftime("%Y%m%d")):
+            print("\nUh Oh! Dumps are not from the future.\n")
+            sys.exit(0)
+
+        dates = args.dates
     else:
-        dates = 20181021   # default testing
+        # Default first day of the current month
+        todayDate = datetime.date.today()
+        dates = todayDate.replace(day=1)
     
     # Projects selection
     proj = []
@@ -90,46 +105,52 @@ def main():
     print('\n', '-' * 50)
 
 
-
+    with urlopen('{}/backup-index.html'.format(dumpsdomain)) as url:
+        html = url.read().decode('utf-8')
+    
+    
     fulldumps = []
-    # Regex to get date from the html page
+    downloadlink = ""
     for l in locale:
         for p in proj:
-            REGEXPROJDATE = r'<a href="(?P<language>%s)(?P<project>%s)/(?P<date>%s)">[^<]+</a>: <span class=\'done\'>Dump complete</span>' % (l, p, dates)
-            m = re.compile(REGEXPROJDATE)
-            # print(m)
-            for match in re.finditer(m, html):
-                # print(match)
-                fulldumps.append([match.group('language'), match.group('project'),match.group('date')])
-    
-    # Exit application if no file can be download.
+            try:
+                downloadlink = '{}/{}{}/{}'.format(dumpsdomain, l, p, dates)
+                r = requests.get(downloadlink)
+                r.raise_for_status()
+                fulldumps.append([l,p,dates])
+                print(downloadlink, '--  Link Ready')
+            except HTTPError:
+                print(downloadlink, '--  Not Exist')
+    print(fulldumps)
+
+    # Exit application if no file can be download
     if fulldumps == []:
         print("\nRequested dumps are not available.\nIf server are updating, try again later.\
         \nEnsure the argument passed are correct.","\n" *3)
         sys.exit(0)
         
     for locale, project, date in fulldumps:
-        print ('-' * 50, '\n', 'Checking', locale, project, date, '\n', '-' * 50)
+        print ('-' * 50, '\n', 'Preparing to download', '\n', '-' * 50)
         time.sleep(1)  # ctrl-c
-        f = urlopen('%s/%s%s/%s/' % (dumpsdomain, locale, project, date))
-        htmlproj = f.read().decode('utf-8')
-        # print (htmlproj)
-        f.close()
+        print(downloadlink)
+        with urlopen(downloadlink) as url:
+            htmlproj = url.read().decode('utf-8')
 
         for dumptypes in ['pages-meta-history\d*\.xml[^\.]*\.7z']:
-            corrupted = False
+            corrupted = True
             maxRetriesCheck = maxretries
-            while (not corrupted) and maxRetriesCheck > 0:
+            while (corrupted) and maxRetriesCheck > 0:
                 maxRetriesCheck -=1
                 # refer "/enwiki/20181101/enwiki-20181101-pages-meta-history1.xml-p26584p28273.7z"
                 # enwiki is have many files, looping is required
-                m = re.compile(r'<a href="(?P<urldump>/%s%s/%s/%s%s-%s-%s)">' %  (locale,project,date,locale,project,date,dumptypes))
+                m = re.compile(r'<a href="/(?P<urldump>%s%s/%s/%s%s-%s-%s)">' %  (locale,project,date,locale,project,date,dumptypes))
                 urldumps = []
                 for match in re.finditer(m, htmlproj):
+                    print(match)
                     urldumps.append('%s/%s' % (dumpsdomain, match.group('urldump')))
                 
                 
-                # print (urldumps)
+                print (urldumps)
                 for urldump in urldumps:
                     dumpfilename = urldump.split('/')[-1]
 
@@ -139,33 +160,32 @@ def main():
                     # wget continue downloadlink log to path with dumpfilename
                     os.system('wget --continue %s -O %s/%s' % (urldump, path, dumpfilename))
 
-                    # # md5check
-                    # os.system('md5sum %s/%s > md5' % (path, dumpfilename))
-                    # f = open('md5', 'r')
-                    # raw = f.read()
-                    # f.close()
-                    # md51 = re.findall(
-                    #     r'(?P<md5>[a-f0-9]{32})\s+%s/%s' % (path, dumpfilename), raw)[0]
-                    # print ((md51))
+                    # md5check IN PROGRESS DO NOT DELETE
+                    os.system('md5sum %s/%s > md5' % (path, dumpfilename))
+                    f = open('md5', 'r')
+                    raw = f.read()
+                    f.close()
+                    md51 = re.findall(
+                        r'(?P<md5>[a-f0-9]{32})\s+%s/%s' % (path, dumpfilename), raw)[0]
+                    print ((md51))
 
-                    # f = urlopen(
-                    #     '%s/%s/%s/%s-%s-md5sums.txt' % (dumpsdomain, project, date, project, date)).decode('utf-8')
-                    # raw = f.read()
-                    # f.close()
-                    # f = open('%s/%s-%s-md5sums.txt' %
-                    #          (path, project, date), 'w')
-                    # f.write(raw)
-                    # f.close()
-                    # md52 = re.findall(
-                    #     r'(?P<md5>[a-f0-9]{32})\s+%s' % (dumpfilename), raw)[0]
-                    # print ((md52))
+                    with urlopen('%s/%s%s/%s/%s%s-%s-md5sums.txt' % (dumpsdomain, locale, project, date, locale, project, date)) as f:
+                        raw = f.read().decode('utf-8')
 
-                    # if md51 == md52:
-                    #     print ('MD5SUM Check Pass!')
-                    #     print ('\n' * 3)
-                    #     corrupted = False
-                    # else:
-                    #     os.remove('%s/%s' % (path, dumpfilename))               
+                    f = open('%s/%s-%s-md5sums.txt' %
+                             (path, project, date), 'w')
+                    f.write(raw)
+                    f.close()
+                    md52 = re.findall(
+                        r'(?P<md5>[a-f0-9]{32})\s+%s' % (dumpfilename), raw)[0]
+                    print ((md52))
+
+                    if md51 == md52:
+                        print ('md5sum Check Pass!')
+                        print ('\n' * 3)
+                        corrupted = False
+                    else:
+                        os.remove('%s/%s' % (path, dumpfilename))               
 
 
 if __name__ == '__main__':
